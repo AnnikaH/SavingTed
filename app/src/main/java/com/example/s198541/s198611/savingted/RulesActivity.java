708909d06a8111bcd53565aaed69dff95adc01ef
AnnikaH <annika_limfe@hotmail.com>
2015-09-01 16:58:11 +0200

    Added layout xml-files for each activity and updated the manifest-file

--- app/src/main/java/com/example/s198541/s198611/savingted/RulesActivity.java
+++ app/src/main/java/com/example/s198541/s198611/savingted/RulesActivity.java
@@ -8,5 +8,6 @@ public class RulesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
     }
 }
