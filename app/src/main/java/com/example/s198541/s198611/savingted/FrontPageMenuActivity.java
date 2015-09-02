c63f49a90f6f031677cbdd7a57c2d261a3a95237
AnnikaH <annika_limfe@hotmail.com>
2015-09-03 01:37:42 +0200

    png background for both menu and game - tried to add gif as background, but not working as is.

--- app/src/main/java/com/example/s198541/s198611/savingted/FrontPageMenuActivity.java
+++ app/src/main/java/com/example/s198541/s198611/savingted/FrontPageMenuActivity.java
@@ -1,10 +1,17 @@
 package com.example.s198541.s198611.savingted;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Movie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.io.InputStream;
 
 public class FrontPageMenuActivity extends AppCompatActivity {
 
@@ -12,7 +19,38 @@ public class FrontPageMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page_menu);
+//      setContentView(new MyGIFView(getApplicationContext()));
+//      getApplicationContext() possibly not working: MyApp.getContext();
+//      MyApp skal tilsvare manifestfilen sin application: android:name="MyApp"
     }
+
+//    private class MyGIFView extends View {
+//        Movie movie;
+//        InputStream is = null;
+//        long moviestart;
+//
+//        public MyGIFView(Context context) {
+//            super(context);
+//            is = context.getResources().openRawResource(R.raw.hangman_background_animation);
+//            movie = Movie.decodeStream(is);
+//        }
+//
+//        @Override
+//        protected void onDraw(Canvas canvas) {
+//            canvas.drawColor(Color.WHITE);
+//            super.onDraw(canvas);
+//            long now = android.os.SystemClock.uptimeMillis();
+//
+//            if(moviestart == 0) {
+//                moviestart = now;
+//            }
+//
+//            int relTime = (int) ((now - moviestart) % movie.duration());
+//            movie.setTime(2000);
+//            movie.draw(canvas, this.getWidth() / 2 - 20, this.getHeight() / 2 - 40);
+//            this.invalidate();
+//        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
