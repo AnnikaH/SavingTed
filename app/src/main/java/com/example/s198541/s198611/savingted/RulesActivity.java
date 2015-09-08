f7be27d24ed1a9d59e8003998c12491633b7f614
KidaSacheil <Kassandra994@live.no>
2015-09-08 08:50:14 +0200

    La til String med regler og oppdaterte Regler activiteten.

--- app/src/main/java/com/example/s198541/s198611/savingted/RulesActivity.java
+++ app/src/main/java/com/example/s198541/s198611/savingted/RulesActivity.java
@@ -12,6 +12,7 @@ public class RulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+
     }
 
     @Override
